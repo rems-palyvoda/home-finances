655bd00229e85c09a9a69fccde98b303cb651da6
Roman Mazurets <rmazurets@blackbird-lab.com>
2019-08-25 01:04:12 +0300

    setup test env

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to have_many :transactions }
+  it { is_expected.to have_many(:transactions).dependent(:destroy) }
 end
