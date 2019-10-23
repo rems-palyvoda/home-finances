8b8479798a9330b23d7647a646cc40a21ea62eab
Roman Mazurets <rmazurets@blackbird-lab.com>
2019-10-23 16:23:09 +0300

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_140820) do
+ActiveRecord::Schema.define(version: 2019_10_23_131413) do
 
   create_table "transactions", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_09_27_140820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "name"
   end
 
 end
